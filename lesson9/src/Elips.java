cd758dc59cee39022e99164c94cea0e66b886774
Maria Matsko <mv.matsko@gmail.com>
2021-10-07 18:57:03 +0300

    add Elips

---
+++ lesson9/src/Elips.java
@@ -0,0 +1,17 @@
+public class Elips extends Geometry {
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
